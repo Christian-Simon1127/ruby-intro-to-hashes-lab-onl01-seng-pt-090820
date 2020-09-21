e9e65e2685fcf70624bec85f37f35bea3512ee3e
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-21 19:51:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,7 @@ def monopoly_with_second_tier
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {}
   monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly
 end
 
 def monopoly_with_third_tier
@@ -36,7 +37,7 @@ def monopoly_with_third_tier
   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
   monopoly[:railroads][:names][:b_and_o_railroad] = {}
   monopoly[:railroads][:names][:shortline] = {}
-  
+  monopoly
 end
 
 def monopoly_with_fourth_tier
